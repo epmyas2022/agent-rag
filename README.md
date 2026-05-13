e2d5fd8a47b38c45a819db27b69fb8178caa0e89
Isaac Castillo <iocastillo@innovacion.gob.sv>
2026-05-13 11:13:27 -0600

    Add initial project files: .gitignore, README, main.py, requirements.txt, and image

---
+++ README.md
@@ -0,0 +1,32 @@
+# RAG (Retrieval-Augmented Generation) Agent Example
+
+Poyecto de ejemplo para un agente RAG (Retrieval-Augmented Generation) utilizando LangChain, Ollama y SentenceTransformer.
+
+## Requisitos
+
+- Python 3.14.3 o superior
+- Ollama (instalado y en ejecución)
+
+## Instalación
+
+1. Clona este repositorio:
+
+   ```bash
+   git clone
+    ```
+
+2. Navega al directorio del proyecto:
+
+    ```bash
+    cd agent-rag
+    ```
+
+3. Instala las dependencias:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Uso
+
+![alt text](image.png)
---
+++ main.py
@@ -0,0 +1,88 @@
+import os
+
+from langchain.agents import create_agent
+from langchain.messages import AIMessageChunk
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import sys
+import asyncio
+from colorama import Fore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pypdf import PdfReader
+
+
+
+def read_pdf(file_path: str) -> str:
+
+    if not os.path.exists(file_path):
+        return None
+    
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+    return text
+
+
+async def main():
+    def get_weather(city: str) -> str:
+        """Get weather for a given city."""
+        return f"It's always sunny in {city}!"
+
+    document = read_pdf("document.pdf")
+
+    if not document:
+        print(f"{Fore.RED}Error: Could not read the PDF file.{Fore.RESET}")
+        exit(1)
+
+    print(document)  # Print the first 500 characters of the document for verification
+    splitter = RecursiveCharacterTextSplitter(chunk_size=350, chunk_overlap=20)
+
+    chunks = splitter.split_text(document)
+
+    model_embeddings = SentenceTransformer("BAAI/bge-m3")
+
+    embeddings = model_embeddings.encode(chunks)
+
+    question = sys.argv[1]
+
+    question_embedding = model_embeddings.encode([question])
+
+    vector_similary = cosine_similarity(question_embedding, embeddings)[0]
+
+    index = np.argmax(vector_similary)
+
+    print(f"\n({Fore.BLUE}Context: {chunks[index]}{Fore.RESET})")
+
+    system_prompt = f"""
+    # Rol: 
+    Eres un asistente y debes responder usando el contexto
+
+    ## Reglas
+
+    - No debes agregar información que no esté en el contexto
+    - Si no sabes la respuesta, responde "No lo sé"
+
+
+    Responde con este contexto:
+    {chunks[index]}
+    """
+
+    agent = create_agent(
+        model="ollama:qwen3.5:4b",
+        tools=[get_weather],
+        system_prompt=system_prompt,
+    )
+
+    print("\n\n\nPregunta:", question)
+
+    async for chunk, metadata in agent.astream(
+        {"messages": [{"role": "user", "content": question}]}, stream_mode="messages"
+    ):
+
+        if isinstance(chunk, AIMessageChunk):
+            print(Fore.YELLOW + chunk.content + Fore.RESET, end="", flush=True)
+
+
+asyncio.run(main())
